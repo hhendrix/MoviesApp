0ed757248d31f7f345722c7d8cd02d91cdb625c1
Harry Lopez <harry.lopez@overactive.com>
2022-08-17 17:05:30 -0500

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Harry Lopez on 17/08/22.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
